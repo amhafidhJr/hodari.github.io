157152436d0c9a4d10ab13ae04fb85af81f25f46
abdulhalim.hafidh <abdulhalim.hafidh@tra.go.tz>
2023-12-18 00:22:24 +0300

    fixing bugs

--- src/app/faqs/faqs/faqs.component.ts
+++ src/app/faqs/faqs/faqs.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Component({
-  selector: 'app-faqs',
-  templateUrl: './faqs.component.html',
-  styleUrls: ['./faqs.component.scss']
+  selector: "app-faqs",
+  templateUrl: "./faqs.component.html",
+  styleUrls: ["./faqs.component.scss"],
 })
 export class FaqsComponent implements OnInit {
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  seconds: number = 0;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   ngOnInit(): void {
-  }
+    const end = new Date("Jan 12, 2024 00:00:00").getTime();
+    const seconds = 1000;
+    const minutes = seconds * 60;
+    const hours = minutes * 60;
+    const days = hours * 24;
 
+    const x = setInterval(() => {
+      let now = new Date().getTime();
+      const difference = end - now;
+
+      if (difference < 0) {
+        clearInterval(x);
+        this.renderer.setProperty(
+          this.el.nativeElement.querySelector("#done"),
+          "innerHTML",
+          "We're married! 🎉"
+        );
+        return;
+      }
+
+      this.days = Math.floor(difference / days);
+      this.hours = Math.floor((difference % days) / hours);
+      this.minutes = Math.floor((difference % hours) / minutes);
+      this.seconds = Math.floor((difference % minutes) / seconds);
+    }, seconds);
+  }
 }
